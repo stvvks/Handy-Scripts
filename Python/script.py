e8ec2d51f8f8ca5ca8916e292133e44ed5c8eab8
Jeremiah Nkumsah <jeremiahnkumsah@gmail.com>
2024-01-08 12:01:37 -0600

    new python w doc

--- Python/script.py
+++ Python/script.py
@@ -1,2 +1,3 @@
-dict = {'6':'9', '9':'6'}
-print(dict.get(str(input("Enter 6 or 9: ")), "Incorrect number"))
+# Script that defines a value (which are 6 and 9) and spits out the other.
+dict = {'6':'9', '9':'6'} #Using dict as a keypair to store values
+print(dict.get(str(input("Enter 6 or 9: ")), "Incorrect number")) #Using print to spit out, str input allows the user to enter in a value and the opposite will be outputted
